4876f289c87347625508701adc5ed272f7a1ffb9
Geert-Jan Klaps <geert-jan@klaps.org>
2022-09-03 13:43:26 +0200

    Base sample copy

---
+++ README.md
@@ -0,0 +1,87 @@
+# Welcome to cap/samples
+
+Find here a collection of samples for the [SAP Cloud Application Programming Model](https://cap.cloud.sap) organized in a simplistic [monorepo setup](samples.md#all-in-one-monorepo). &rarr; See [**Overview** of contained samples](samples.md)
+
+![](https://github.com/SAP-samples/cloud-cap-samples/workflows/CI/badge.svg)
+[![REUSE status](https://api.reuse.software/badge/github.com/SAP-samples/cloud-cap-samples)](https://api.reuse.software/info/github.com/SAP-samples/cloud-cap-samples)
+
+### Preliminaries
+
+1. Ensure you have the latest LTS version of Node.js installed (see [Getting Started](https://cap.cloud.sap/docs/get-started/))
+2. Install [**@sap/cds-dk**](https://cap.cloud.sap/docs/get-started/) globally:
+
+   ```sh
+   npm i -g @sap/cds-dk
+   ```
+
+3. _Optional:_ [Use Visual Studio Code](https://cap.cloud.sap/docs/get-started/tools#vscode)
+
+### Download
+
+If you've [Git](https://git-scm.com/downloads) installed, clone this repo as shown below, otherwise [download as ZIP file](archive/main.zip).
+
+```sh
+git clone https://github.com/sap-samples/cloud-cap-samples samples
+cd samples
+```
+
+### Setup
+
+In the samples folder run:
+
+```sh
+npm install
+```
+
+### Run
+
+With that you're ready to run the samples, for example:
+
+```sh
+cds watch bookshop
+```
+
+After that open this link in your browser: [http://localhost:4004](http://localhost:4004)
+
+When asked to log in, type `alice` as user and leave the password field blank, which is the [default user](https://cap.cloud.sap/docs/node.js/authentication#mocked).
+
+### Testing
+
+Run the provided tests with [_jest_](http://jestjs.io) or [_mocha_](http://mochajs.org), for example:
+
+```sh
+npx jest
+```
+> While mocha is a bit smaller and faster, jest runs tests in parallel and isolation, which allows to run all tests.
+
+
+### Serve `npm`
+
+We've included a simple npm registry mock, which allows you to do an `npm install @capire/<package>` locally. Use it as follows:
+
+1. Start the @capire registry:
+```sh
+npm run registry
+```
+> While running this will have `@capire:registry=http://localhost:4444` set with npmrc.
+
+2. Install one of the @capire packages wherever you like, for example:
+
+```sh
+npm add @capire/common @capire/bookshop
+```
+
+
+## Code Tours
+
+Take one of the [guided tours](.tours) in VS Code through our CAP samples and learn which CAP features are showcased by the different parts of the repository. Just install the [CodeTour extension](https://marketplace.visualstudio.com/items?itemName=vsls-contrib.codetour) for VS Code. We'll add more code tours in the future. Stay tuned!
+
+## Get Support
+
+Check out the documentation at [https://cap.cloud.sap](https://cap.cloud.sap). <br>
+In case you've a question, find a bug, or otherwise need support, use our [community](https://answers.sap.com/tags/9f13aee1-834c-4105-8e43-ee442775e5ce) to get more visibility.
+
+
+## License
+
+Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, version 2.0 except as noted otherwise in the [LICENSE](LICENSE) file.
---
+++ test/odata.test.js
@@ -0,0 +1,139 @@
+const cds = require('@sap/cds/lib')
+
+describe('cap/samples - Bookshop APIs', () => {
+  const { GET, expect, axios } = cds.test ('@capire/bookshop')
+  axios.defaults.auth = { username: 'alice', password: 'admin' }
+
+  // Genres
+  const Drama = {
+    "name": "Drama",
+    "descr": null,
+    "ID": 11,
+    "parent_ID": 10
+  }
+  const Mystery = {
+    "name": "Mystery",
+    "descr": null,
+    "ID": 16,
+    "parent_ID": 10
+  }
+  const Fantasy = {
+    "name": "Fantasy",
+    "descr": null,
+    "ID": 13,
+    "parent_ID": 10
+  }
+
+  // Currencies
+  const GBP = {
+    "name": "British Pound",
+    "descr": null,
+    "code": "GBP",
+    "symbol": "£"
+  }
+  const USD = {
+    "name": "US Dollar",
+    "descr": null,
+    "code": "USD",
+    "symbol": "$"
+  }
+  const JPY = {
+    "name": "Yen",
+    "descr": null,
+    "code": "JPY",
+    "symbol": "¥"
+  }
+
+
+  it('serves $metadata documents in v4', async () => {
+    const { headers, status, data } = await GET `/browse/$metadata`
+    expect(status).to.equal(200)
+    expect(headers).to.contain({
+      'content-type': 'application/xml',
+      'odata-version': '4.0',
+    })
+    expect(data).to.contain('<EntitySet Name="Books" EntityType="CatalogService.Books">')
+    expect(data).to.contain('<Annotation Term="Common.Label" String="Currency"/>')
+  })
+
+  it('serves ListOfBooks?$expand=genre,currency', async () => {
+    const { data } = await GET `/browse/ListOfBooks ${{
+      params: { $search: 'Po', $select: `title,author`, $expand:`genre,currency` },
+    }}`
+    expect(data.value).to.eql([
+      { ID: 251, title: 'The Raven', author: 'Edgar Allen Poe', genre:Mystery, currency:USD  },
+      { ID: 252, title: 'Eleonora', author: 'Edgar Allen Poe', genre:Mystery, currency:USD  },
+    ])
+  })
+
+  it('supports $search in multiple fields', async () => {
+    const { data } = await GET `/browse/Books ${{
+      params: { $search: 'Po', $select: `title,author` },
+    }}`
+    expect(data.value).to.eql([
+      { ID: 201, title: 'Wuthering Heights', author: 'Emily Brontë' },
+      { ID: 207, title: 'Jane Eyre', author: 'Charlotte Brontë' },
+      { ID: 251, title: 'The Raven', author: 'Edgar Allen Poe' },
+      { ID: 252, title: 'Eleonora', author: 'Edgar Allen Poe' },
+    ])
+  })
+
+  it('supports $select', async () => {
+    const { data } = await GET(`/browse/Books`, {
+      params: { $select: `ID,title` },
+    })
+    expect(data.value).to.eql([
+      { ID: 201, title: 'Wuthering Heights' },
+      { ID: 207, title: 'Jane Eyre' },
+      { ID: 251, title: 'The Raven' },
+      { ID: 252, title: 'Eleonora' },
+      { ID: 271, title: 'Catweazle' },
+    ])
+  })
+
+  it('supports $expand', async () => {
+    const { data } = await GET(`/admin/Authors`, {
+      params: {
+        $select: `name`,
+        $expand: `books($select=title)`,
+      },
+    })
+    expect(data.value).to.containSubset([
+      { name: 'Emily Brontë', books: [{ title: 'Wuthering Heights' }] },
+      { name: 'Charlotte Brontë', books: [{ title: 'Jane Eyre' }] },
+      { name: 'Edgar Allen Poe', books: [{ title: 'The Raven' }, { title: 'Eleonora' }] },
+      { name: 'Richard Carpenter', books: [{ title: 'Catweazle' }] },
+    ])
+  })
+
+  it('supports $value requests', async () => {
+    const { data } = await GET`/admin/Books/201/stock/$value`
+    expect(data).to.equal(12)
+  })
+
+  it('supports $top/$skip paging', async () => {
+    const { data: p1 } = await GET`/browse/Books?$select=title&$top=3`
+    expect(p1.value).to.eql([
+      { ID: 201, title: 'Wuthering Heights' },
+      { ID: 207, title: 'Jane Eyre' },
+      { ID: 251, title: 'The Raven' },
+    ])
+    const { data: p2 } = await GET`/browse/Books?$select=title&$skip=3`
+    expect(p2.value).to.eql([
+      { ID: 252, title: 'Eleonora' },
+      { ID: 271, title: 'Catweazle' },
+    ])
+  })
+
+  it('serves user info', async () => {
+    {
+      const { data } = await GET (`/user/me`)
+      expect(data).to.containSubset({ id: 'alice', locale:'en', tenant: null })
+    }
+    {
+      const { data } = await GET (`/user/me`, {auth: { username: 'joe' }})
+      expect(data).to.containSubset({ id: 'joe', locale:'en', tenant: null })
+    }
+  })
+
+})
